d38689738a009dcb83a756174608ccd483e3a233
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2020-10-04 11:14:15 +0530

    Fixed 🔧

--- plugins/gofile.py
+++ plugins/gofile.py
@@ -84,7 +84,7 @@ def get_link(bot, update):
             adfulurl = file_inance.webContentLink
             max_days = 0
         else: 
-            url = "https://srv-store5.gofile.io/upload"
+            url = "https://srv-store5.gofile.io/uploadFile"
             max_days = 5
             timeseconds = int(time.time())
             timesecondsplusexpiry = int(time.time()) + (max_days*24*60*60)
